bbb957e765a92a72292f4a0d1d55c25a3053cb45
hadilouakkouche <hadil@shift89.fr>
2025-06-20 19:23:10 +0200

    _

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Details from './pages/Details';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/datasets/:id" element={<Details />} />
       </Routes>
     </Router>
   );
